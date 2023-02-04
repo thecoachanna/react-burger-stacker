60eaf4b748a62682f02fce06847763494001a9a1
Anna Nettles <99093454+thecoachanna@users.noreply.github.com>
2023-02-04 13:01:32 -0500

    added component files

---
+++ .history/src/components/BurgerStack_20230204125658.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BurgerStack = () => {
+  return (
+    <div>BurgerStack</div>
+  )
+}
+
+export default BurgerStack
---
+++ .history/src/components/IngredientList_20230204125651.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IngredientList = () => {
+  return (
+    <div>IngredientList</div>
+  )
+}
+
+export default IngredientList
